645841740482562653c0b0a72654ab0770237510
111vamsi <73297558+111vamsi@users.noreply.github.com>
2024-07-26 19:30:49 +0530

    Add files via upload

---
+++ Number greater than 1000.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a numeric integer: ");
+
+        try
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0 || number > 1000)
+            {
+                throw new Exception("The number must be between 0 and 1000.");
+            }
+
+            Console.WriteLine($"You entered: {number}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Word of a string.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+
+        // Split the string into words using spaces as the delimiter
+        string[] words = input.Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Count the number of words
+        int wordCount = words.Length;
+
+        Console.WriteLine($"Total number of words: {wordCount}");
+        Console.ReadLine();
+    }
+    
+}
